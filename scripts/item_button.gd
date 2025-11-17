62b3ee59736b4fe164aa5785cd12e2fc83e28ec3
nadia-nh <nadia.heredia.nh@gmail.com>
2025-11-16 23:07:26 -0500

    Add crop data and tools to the item buttons, and set values

--- scripts/item_button.gd
+++ scripts/item_button.gd
@@ -1,5 +1,14 @@
 extends TextureButton
 
+@export var crop_data : CropData
+@export var tool : ItemData.Tool
+
+var player_item : ItemData.Item
+
+func _init() -> void:
+	var player_seed = ItemData.Seed.new(crop_data)
+	player_item = ItemData.Item.new(tool, player_seed)
+
 func _ready() -> void:
 	pass
 

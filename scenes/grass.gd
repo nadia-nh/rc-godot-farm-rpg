270ca3f4a8015624e03e13611633b78739eb3f0a
nadia-nh <nadia.heredia.nh@gmail.com>
2025-11-16 23:35:21 -0500

    Make item button delegate actions to Grass class

--- scenes/grass.gd
+++ scenes/grass.gd
@@ -1,3 +1,15 @@
 class_name Grass
 
 extends Node2D
+
+func till_tile():
+	pass
+
+func water_tile():
+	pass
+	
+func harvest_tile():
+	pass
+	
+func plant_tile(_crop : CropData):
+	pass
--- scripts/item_button.gd
+++ scripts/item_button.gd
@@ -3,14 +3,25 @@ extends TextureButton
 @export var crop_data : CropData
 @export var tool : ItemData.Tool
 
-var player_item : ItemData.Item
+var _player_item : ItemData.Item
+
+@onready var _grass := $"../../../Grass"
 
 func _init() -> void:
 	var player_seed = ItemData.Seed.new(crop_data)
-	player_item = ItemData.Item.new(tool, player_seed)
+	_player_item = ItemData.Item.new(tool, player_seed)
 
 func _ready() -> void:
 	pass
 
+# Use the item, when there's no tool it means we have a seed
 func _on_pressed() -> void:
-	pass
+	match _player_item.toolData:
+		ItemData.Tool.HOE:
+			_grass.till_tile()
+		ItemData.Tool.SCYTHE:
+			_grass.harvest_tile()
+		ItemData.Tool.WATER_BUCKET:
+			_grass.water_tile()
+		ItemData.Tool.NONE:
+			_grass.plant_tile(crop_data)

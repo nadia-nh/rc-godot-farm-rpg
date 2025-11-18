46256273916b463370108e641667fd4061155da8
nadia-nh <nadia.heredia.nh@gmail.com>
2025-11-17 19:36:46 -0500

    Add GameManager autoload script to handle day changes
    Adds a signal for day_changed which other classes can subscribe to

---
+++ scripts/game_manager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+signal day_changed()
+
+var _current_day : int
+
+func _ready() -> void:
+	_current_day = 0
+	_set_next_day.call_deferred()
+	
+func get_current_day() -> int:
+	return _current_day
+
+# Increases the day, and emits the corresponding signal
+func _set_next_day():
+	_current_day += 1
+	day_changed.emit()

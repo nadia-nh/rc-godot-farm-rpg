bcdccf1ccd9a325c7663cccf9d6420f62ddcc816
nadia-nh <nadia.heredia.nh@gmail.com>
2025-11-21 13:42:00 -0500

    Use `is_action_just_pressed` instead of `is_action_pressed` in InputDispatcher
    This makes sure that we only handle the key presses once instead of multiple times

--- scripts/input_dispatcher.gd
+++ scripts/input_dispatcher.gd
@@ -4,9 +4,9 @@ func _ready() -> void:
 	pass
 
 func _physics_process(_delta):
-	if Input.is_action_pressed("next_day"):
+	if Input.is_action_just_pressed("next_day"):
 		GameManager.day_changed.emit()
-	if Input.is_action_pressed("use_item"):
+	if Input.is_action_just_pressed("use_item"):
 		GameManager.item_used.emit()
 	
 	if Input.is_action_just_pressed("tool_hoe"):

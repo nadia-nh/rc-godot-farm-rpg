7d00539ed6a114acdce2f0d79b4043053c3e5b54
nadia-nh <nadia.heredia.nh@gmail.com>
2025-11-24 11:16:46 -0500

    Connect to pressed signal on ItemButton from script code

--- scripts/item_button.gd
+++ scripts/item_button.gd
@@ -1,3 +1,4 @@
+class_name ItemButton
 extends TextureButton
 
 @export var crop_resource : CropResource
@@ -9,9 +10,10 @@ func _init() -> void:
 	pass
 
 func _ready() -> void:
+	pressed.connect(_on_mouse_pressed)
 	var player_seed = ItemData.Seed.new(crop_resource)
 	_player_item = ItemData.Item.new(tool, player_seed)
 
 # Use the item, when there's no tool it means we have a seed
-func _on_pressed() -> void:
+func _on_mouse_pressed() -> void:
 	GameManager.select_item(_player_item)

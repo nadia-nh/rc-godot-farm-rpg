fc8904b6e9429d5d53a8d3bb4a452b7725c6f768
nadia-nh <nadia.heredia.nh@gmail.com>
2025-11-24 11:23:00 -0500

    Color buttons when they are selected

--- scripts/item_button.gd
+++ scripts/item_button.gd
@@ -14,6 +14,12 @@ func _ready() -> void:
 	var player_seed = ItemData.Seed.new(crop_resource)
 	_player_item = ItemData.Item.new(tool, player_seed)
 
+func show_button_selected() -> void:
+	self.self_modulate = Color("#F7AC38")
+
+func show_button_unselected() -> void:
+	self.self_modulate = Color("#FFFFFF")
+
 # Use the item, when there's no tool it means we have a seed
 func _on_mouse_pressed() -> void:
 	GameManager.item_selected.emit(_player_item)
--- scripts/ui_layer.gd
+++ scripts/ui_layer.gd
@@ -1,2 +1,30 @@
 extends CanvasLayer
 
+@onready var buttons_container = $ItemButtonContainer
+var item_buttons : Array[ItemButton]
+
+func _ready() -> void:
+	GameManager.item_selected.connect(_on_item_selected)
+	for child in buttons_container.get_children():
+		if child is ItemButton:
+			item_buttons.append(child)
+
+func _on_item_selected(item: ItemData.Item) -> void:
+	for button in item_buttons:
+		if _button_has_item(button, item):
+			button.show_button_selected()
+		else:
+			button.show_button_unselected()
+
+func _button_has_item(button: ItemButton, item: ItemData.Item) -> bool:
+	return (_button_has_same_tool(button, item) and
+			_button_has_same_seed(button, item))
+
+func _button_has_same_tool(button: ItemButton, item: ItemData.Item) -> bool:
+	return button.tool == item.tool_data
+
+func _button_has_same_seed(button: ItemButton, item: ItemData.Item) -> bool:
+	if (item.tool_data != ItemData.Tool.NONE):
+		return true
+	
+	return item.seed_data.crop_resource == button.crop_resource

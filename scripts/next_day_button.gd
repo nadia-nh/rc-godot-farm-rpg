c698d5605253fa160ed9b6ad8b3827c3a365d1dc
nadia-nh <nadia.heredia.nh@gmail.com>
2025-11-24 19:21:14 -0500

    Briefly color the next day button when advancing the day

--- scripts/next_day_button.gd
+++ scripts/next_day_button.gd
@@ -6,3 +6,8 @@ func _ready() -> void:
 func _on_pressed() -> void:
 	GameManager.day_changed.emit()
 
+func show_button_selected() -> void:
+	self.self_modulate = Color("#F7AC38")
+
+func show_button_unselected() -> void:
+	self.self_modulate = Color("#FFFFFF")
--- scripts/ui_layer.gd
+++ scripts/ui_layer.gd
@@ -1,10 +1,13 @@
 extends CanvasLayer
 
-@onready var buttons_container = $ItemButtonContainer
 var item_buttons : Array[ItemButton]
+
+@onready var buttons_container = $ItemButtonContainer
+@onready var next_day_button = $NextDayButton
 
 func _ready() -> void:
 	GameManager.item_selected.connect(_on_item_selected)
+	GameManager.day_changed.connect(_on_day_changed)
 	for child in buttons_container.get_children():
 		if child is ItemButton:
 			item_buttons.append(child)
@@ -15,6 +18,10 @@ func _on_item_selected(item: ItemData.Item) -> void:
 			button.show_button_selected()
 		else:
 			button.show_button_unselected()
+
+func _on_day_changed() -> void:
+	next_day_button.show_button_selected()
+	get_tree().create_timer(0.1).timeout.connect(next_day_button.show_button_unselected)
 
 func _button_has_item(button: ItemButton, item: ItemData.Item) -> bool:
 	return (_button_has_same_tool(button, item) and

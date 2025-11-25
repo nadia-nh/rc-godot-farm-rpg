5ea35f3a51effd6435e47884682c1ea867963fa7
nadia-nh <nadia.heredia.nh@gmail.com>
2025-11-25 11:59:08 -0500

    Display player's money in the money display

--- scripts/money_display.gd
+++ scripts/money_display.gd
@@ -1,2 +1,9 @@
 extends TextureRect
 
+@onready var money_text = $MoneyText
+
+func _ready() -> void:
+	GameManager.money_updated.connect(_on_money_updated)
+
+func _on_money_updated(quantity: int) -> void:
+	money_text.text = str(quantity)

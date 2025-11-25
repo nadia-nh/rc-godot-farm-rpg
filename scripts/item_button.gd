cd2e558fcb5034e0db1071fb31ca3f4f8aa66cc7
nadia-nh <nadia.heredia.nh@gmail.com>
2025-11-24 20:01:20 -0500

    Connect to seed quantity updated signal and change the quantity text

--- scripts/item_button.gd
+++ scripts/item_button.gd
@@ -5,6 +5,7 @@ extends TextureButton
 @export var tool : ItemData.Tool
 
 var _player_item : ItemData.Item
+@onready var quantity_text : Label = $Text
 
 func _init() -> void:
 	pass
@@ -19,6 +20,9 @@ func show_button_selected() -> void:
 
 func show_button_unselected() -> void:
 	self.self_modulate = Color("#FFFFFF")
+
+func update_quantity_text(quantity: int) -> void:
+	quantity_text.text = str(quantity)
 
 # Use the item, when there's no tool it means we have a seed
 func _on_mouse_pressed() -> void:
--- scripts/ui_layer.gd
+++ scripts/ui_layer.gd
@@ -8,6 +8,7 @@ var item_buttons : Array[ItemButton]
 func _ready() -> void:
 	GameManager.item_selected.connect(_on_item_selected)
 	GameManager.day_changed.connect(_on_day_changed)
+	GameManager.seed_quantity_updated.connect(_on_seed_quantity_updated)
 	for child in buttons_container.get_children():
 		if child is ItemButton:
 			item_buttons.append(child)
@@ -21,11 +22,25 @@ func _on_item_selected(item: ItemData.Item) -> void:
 
 func _on_day_changed() -> void:
 	next_day_button.show_button_selected()
-	get_tree().create_timer(0.1).timeout.connect(next_day_button.show_button_unselected)
+	var timer = get_tree().create_timer(0.1)
+	timer.timeout.connect(next_day_button.show_button_unselected)
+
+func _on_seed_quantity_updated(crop_resource: CropResource, quantity: int) -> void:
+	for button in item_buttons:
+		if _button_has_seed(button, crop_resource):
+			button.update_quantity_text(quantity)
 
 func _button_has_item(button: ItemButton, item: ItemData.Item) -> bool:
 	return (_button_has_same_tool(button, item) and
 			_button_has_same_seed(button, item))
+
+func _button_has_valid_seed(button: ItemButton) -> bool:
+	return (button.tool == ItemData.Tool.NONE and
+			is_instance_valid(button.crop_resource))
+
+func _button_has_seed(button: ItemButton, crop_resource: CropResource) -> bool:
+	return (_button_has_valid_seed(button) and
+			button.crop_resource == crop_resource)
 
 func _button_has_same_tool(button: ItemButton, item: ItemData.Item) -> bool:
 	return button.tool == item.tool_data
@@ -34,4 +49,4 @@ func _button_has_same_seed(button: ItemButton, item: ItemData.Item) -> bool:
 	if (item.tool_data != ItemData.Tool.NONE):
 		return true
 	
-	return item.seed_data.crop_resource == button.crop_resource
+	return _button_has_seed(button, item.seed_data.crop_resource)

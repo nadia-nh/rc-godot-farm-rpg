9d3333920c404f01f6def73290e8ec91876b7301
nadia-nh <nadia.heredia.nh@gmail.com>
2025-11-26 22:10:42 -0500

    Make sure that the money display always fits in the asset
    The new _on_money_updated method caps the number of characters in the money text at 5, including the dollar sign at the beginning

--- scripts/money_display.gd
+++ scripts/money_display.gd
@@ -4,10 +4,30 @@ extends TextureRect
 ##
 ## Updates the displayed money amount when the player's money changes.
 
+const MAX_QUANTITY: int = 10_000_000
+
 @onready var money_text = $MoneyText
 
 func _ready() -> void:
 	GameManager.money_updated.connect(_on_money_updated)
 
+# Shows the player's money (capped at 10 million) and formats it so
+# the text fits within the width of the asset.
 func _on_money_updated(quantity: int) -> void:
-	money_text.text = str(quantity)
+	var num = min(quantity, MAX_QUANTITY)
+	var millions = num / 1_000_000.0
+	var thousands = num / 1_000.0
+	var text = "$"
+
+	if num == MAX_QUANTITY:
+		text += "10m"
+	elif millions >= 1:
+		text += str(millions).pad_decimals(1) + "m"
+	elif thousands > 9:
+		text += str(int(thousands)) + "k"
+	elif thousands > 1:
+		text += str(thousands).pad_decimals(1) + "k"
+	else:
+		text += str(quantity)
+
+	money_text.text = text

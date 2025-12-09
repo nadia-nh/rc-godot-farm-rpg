bc0be15e455666e96c6e0a5197dfbffd476f25bd
nadia-nh <nadia.heredia.nh@gmail.com>
2025-12-09 14:54:17 -0500

    Allow closing the game when pressing the q key in the Menu scene

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,6 +5,13 @@ extends Control
 func _ready() -> void:
 	_exit_button.pressed.connect(_on_exit_button_pressed)
 
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("quit_game"):
+		_quit_game()
+
 func _on_exit_button_pressed() -> void:
+	_quit_game()
+
+func _quit_game() -> void:
 	# TODO: Add confirmation screen
 	get_tree().quit()

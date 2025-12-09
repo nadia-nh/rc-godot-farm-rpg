b891fdda696fe0e35955257d07af0dc1792a046a
nadia-nh <nadia.heredia.nh@gmail.com>
2025-12-09 14:52:50 -0500

    Close the game when pressing the quit icon in the Menu scene

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,4 +1,10 @@
 extends Control
 
+@onready var _exit_button = $ExitButton
+
 func _ready() -> void:
-	pass
+	_exit_button.pressed.connect(_on_exit_button_pressed)
+
+func _on_exit_button_pressed() -> void:
+	# TODO: Add confirmation screen
+	get_tree().quit()

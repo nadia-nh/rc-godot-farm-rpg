f268e300264e3f7c52cd05362f11d009ef5fd254
nadia-nh <nadia.heredia.nh@gmail.com>
2025-12-09 21:05:33 -0500

    Continue / start the game when pressing the middle icon in the Menu scene
    This is very slow but it works

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,13 +1,20 @@
 extends Control
 
+@onready var _main_scene : PackedScene = preload("res://scenes/main.tscn")
+@onready var _continue_button = $ContinueButton
 @onready var _exit_button = $ExitButton
 
 func _ready() -> void:
+	_continue_button.pressed.connect(_on_continue_button_pressed)
 	_exit_button.pressed.connect(_on_exit_button_pressed)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("quit_game"):
 		_quit_game()
+
+func _on_continue_button_pressed() -> void:
+	# TODO: Make transition faster
+	get_tree().change_scene_to_packed(_main_scene)
 
 func _on_exit_button_pressed() -> void:
 	_quit_game()

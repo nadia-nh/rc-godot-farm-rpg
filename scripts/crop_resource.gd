64da81ac41d53f3d70a3707369f8b08c7f699504
nadia-nh <nadia.heredia.nh@gmail.com>
2026-01-22 16:07:17 -0500

    Add a name to the CropResource class

--- scripts/crop_resource.gd
+++ scripts/crop_resource.gd
@@ -3,5 +3,6 @@ class_name CropResource
 extends Resource
 
 @export var crop_assets: Array[Texture]
+@export var crop_name: String
 @export var seed_price: int = 10
 @export var sell_price: int = 20
